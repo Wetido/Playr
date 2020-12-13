d62318028fcca15e29d33d7f26be0d60637797cc
Wetido <wetido@gmail.com>
2020-12-13 19:05:17 +0100

    Simple audio player

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Playlist from 'react-mp3-player';
+import ReactHowler from 'react-howler'
 
 
-function App() {
+const App = () => {
+
+  const [audio, setAudio] = useState(new Audio("https://goldfirestudios.com/proj/howlerjs/sound.ogg"))
+
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false)
+
+
+  const changeAudioHandler = () => {
+    
+    setIsAudioPlaying(!isAudioPlaying)
+  }
+
+  useEffect(() => {
+    if(isAudioPlaying){
+      audio.play()
+    }else{
+      audio.pause()
+    }
+
+  }, [isAudioPlaying])
+
+
   return (
     <div>
-      Hello world
+      {isAudioPlaying === true ? 
+        <button onClick={changeAudioHandler}>Stop</button> 
+        : <button onClick={changeAudioHandler}>Start</button>}
     </div>
   );
 }
