89a29f1b898143665c06e43c2b45e71d8fc5bbe5
Wetido <wetido@gmail.com>
2020-12-17 17:27:08 +0100

    Getting song images

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,24 +1,45 @@
 import React, {useState, useEffect, useContext } from 'react';
-
+import base from "../db/firebase"
+import 'firebase/storage';
+import "../css/song-list.css"
 
 const Navbar = (props) => {
 
   
     const [songs, setSongs] = useState([])
+    const [coverImageUrls, setCoverImageUrls] = useState([])
 
+    const getSongLink = async () =>{
+
+        try {
+            const tempList = []
+            for(let i = 0; i < songs.length; i++){
+                const storageRef = base.storage().ref();
+                const fileRef = storageRef.child(songs[i].cover_image_uuid)
+                tempList.push(await fileRef.getDownloadURL())
+            }
+            setCoverImageUrls(tempList)
+        
+        }catch(error){}
+    }
 
     useEffect(() => {
 
         setSongs(props.songs)
-        
-
     },[props.songs])
 
+    useEffect(() =>{
+
+        getSongLink()
+    },[songs])
 
     return (
-        <div>
+        <div className="song-list-wrapper">
             {songs.length !== 0 ? songs.map((song, i) => (
-                <div onClick={() => props.handleChangeSong(i)}>{song.name}</div>
+                <div className="song-wrapper"onClick={() => props.handleChangeSong(i)}>
+                    <img className="cover-image" src={coverImageUrls[i]}></img>
+                    <div className="song-name">{song.name}</div>
+                </div>
             )) : null}
         </div>
         
